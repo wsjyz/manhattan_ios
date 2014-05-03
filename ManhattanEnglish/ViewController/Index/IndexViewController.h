5c2fd8bd5b94f313987e4e5048bedd0581904c7e
AlexanderEric <tantianyu@163.com>
2014-05-03 22:40:48 +0800

    add index view and controllers

---
+++ ManhattanEnglish/ViewController/Index/IndexViewController.h
@@ -0,0 +1,13 @@
+//
+//  IndexViewController.h
+//  ManhattanEnglish
+//
+//  Created by Tianyu Tan on 14-5-3.
+//  Copyright (c) 2014年 8hinfo. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface IndexViewController : BaseViewController
+
+@end
---
+++ ManhattanEnglish/ViewController/Index/IndexViewController.m
@@ -0,0 +1,62 @@
+//
+//  IndexViewController.m
+//  ManhattanEnglish
+//
+//  Created by Tianyu Tan on 14-5-3.
+//  Copyright (c) 2014年 8hinfo. All rights reserved.
+//
+
+#import "IndexViewController.h"
+
+@interface IndexViewController ()
+
+@end
+
+@implementation IndexViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        self.hidesBottomBarWhenPushed = YES;
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        // Custom initialization
+        self.hidesBottomBarWhenPushed = YES;
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self setNavgationItemTitle:@"曼哈顿英语"];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
