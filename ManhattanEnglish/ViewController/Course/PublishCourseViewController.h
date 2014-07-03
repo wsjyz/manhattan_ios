cf954a120bff3b5ebf0994c3278110632351dfa6
Alex <tantianyu@tantianyudeMacBook-Pro.local>
2014-07-04 01:40:44 +0800

    rename group GoodCourse to Course Add PublishCourse ViewController

---
+++ ManhattanEnglish/ViewController/Course/PublishCourseViewController.h
@@ -0,0 +1,14 @@
+//
+//  PublishCourseViewController.h
+//  ManhattanEnglish
+//
+//  Created by Alex on 7/4/14.
+//  Copyright (c) 2014 8hinfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+
+@interface PublishCourseViewController : BaseViewController
+
+@end
