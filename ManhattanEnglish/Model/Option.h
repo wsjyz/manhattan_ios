960bd60bffe0409bab101d370aa814982e46a3be
Alex <tantianyu@tantianyudeMacBook-Pro.local>
2014-07-05 15:57:09 +0800

    add option class

---
+++ ManhattanEnglish/Model/Option.h
@@ -0,0 +1,23 @@
+//
+//  Option.h
+//  ManhattanEnglish
+//
+//  Created by Alex on 7/5/14.
+//  Copyright (c) 2014 8hinfo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Option : NSObject
+
+@property (strong, nonatomic) NSString *text;
+@property (strong, nonatomic) NSString *value;
+
++ (id) optionWithText:(NSString *)text andValue:(NSString *)value;
+
+// key: displayText value:value
++ (NSArray *) optionsWithTextAndValueDic:(NSDictionary *)dic;
+
++ (NSArray *) optionsWithSameTextAndValues:(NSArray *)values;
+
+@end
